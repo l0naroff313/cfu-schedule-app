d53baa5018d4b771fad8ea54b1c4b8abb7d5bf9d
l0naroff313 <l0naroff313@users.noreply.github.com>
2026-09-07 17:28:59 +0300

    chore: align client version with v1.0.4

--- src/UniversitySchedule.Mobile/MauiProgram.cs
+++ src/UniversitySchedule.Mobile/MauiProgram.cs
@@ -53,7 +53,7 @@ public static class MauiProgram
                 client.BaseAddress = apiOptions.BaseAddress;
             }
 
-            client.DefaultRequestHeaders.UserAgent.ParseAdd("CFU-ElJournal/1.0.3");
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("CFU-ElJournal/1.0.4");
             return new UniversityScheduleApiClient(
                 client,
                 apiOptions,
@@ -84,7 +84,7 @@ public static class MauiProgram
                 BaseAddress = new Uri(CfuScheduleRepository.BaseAddress),
                 Timeout = TimeSpan.FromSeconds(15),
             };
-            client.DefaultRequestHeaders.UserAgent.ParseAdd("CFU-ElJournal/1.0.3");
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("CFU-ElJournal/1.0.4");
             return client;
         });
         builder.Services.AddSingleton<CfuScheduleRepository>();
--- src/UniversitySchedule.Web/Program.cs
+++ src/UniversitySchedule.Web/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddScoped<DailyScheduleRefreshService>();
 builder.Services.AddScoped<InstallationIdentityService>();
 
 Uri? apiBaseAddress = TryGetHttpsUri(builder.Configuration["UniversityScheduleApi:BaseUrl"]);
-var apiOptions = new UniversityScheduleApiOptions(apiBaseAddress, "web", "1.0.3");
+var apiOptions = new UniversityScheduleApiOptions(apiBaseAddress, "web", "1.0.4");
 builder.Services.AddSingleton(apiOptions);
 builder.Services.AddScoped(services => new UniversityScheduleApiClient(
     new HttpClient
